d1566e39327545d92ddd05f53591cfe19954a00b
Daniel Thompson <thompson9451@gmail.com>
2022-07-25 16:09:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,2 @@
-from ast import Return
-from urllib import response
-from quip_remove_user import remove_user
-from quip_add_users import add_user
-
-decision=""
-email=""
-nickName=""
-lastName=""
-def main(decision, email, nickName, lastName):
-    if decision == "onboard":
-        add_user(nickName, lastName, email)
-    elif decision == "offboard":
-        remove_user(email)
-    else:
-        print("no way my guy")
-main(decision, email, nickName, lastName)
+ if __name__== "__main__":
+  print("Hello World from the Devops repo!")
